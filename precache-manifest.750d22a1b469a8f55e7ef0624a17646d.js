a5ffa2e931a7b294b7f9f859d65705e628540609
Kamal Deepak <kamaldsharma89@gmail.com>
2020-05-18 08:16:34 +0530

    Updates

---
+++ precache-manifest.750d22a1b469a8f55e7ef0624a17646d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053200a4488bb964867531e7c28e963d",
+    "url": "/Kamal114/myreact/index.html"
+  },
+  {
+    "revision": "02e1762de28912be5237",
+    "url": "/Kamal114/myreact/static/js/2.82bb3967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kamal114/myreact/static/js/2.82bb3967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca2247b9c9b9c4e719a",
+    "url": "/Kamal114/myreact/static/js/main.107bc15c.chunk.js"
+  },
+  {
+    "revision": "b8bc325314e6a8506c54",
+    "url": "/Kamal114/myreact/static/js/runtime-main.9fa7405b.js"
+  }
+]);
